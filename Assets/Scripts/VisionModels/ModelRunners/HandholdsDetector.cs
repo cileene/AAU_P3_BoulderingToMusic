365ed767952b6c46ec5197b012534d548355f633
ValdemarWessing <iq62FM@student.aau.dk>
2025-11-21 18:00:17 +0100

    Flip horisontal y in boxML to make the keypoints and handholds in the same koordinate system

--- Assets/Scripts/VisionModels/ModelRunners/HandholdsDetector.cs
+++ Assets/Scripts/VisionModels/ModelRunners/HandholdsDetector.cs
@@ -262,7 +262,7 @@ namespace VisionModels.ModelRunners
                 var box = new BoundingBox
                 {
                     CenterX = output[n, 0] * scaleX - displayWidth / 2,
-                    CenterY = output[n, 1] * scaleY - displayHeight / 2,
+                    CenterY = -(output[n, 1] * scaleY - displayHeight / 2),
                     Width = output[n, 2] * scaleX,
                     Height = output[n, 3] * scaleY,
                     Label = label,
@@ -337,7 +337,7 @@ namespace VisionModels.ModelRunners
         {
             var panel = AnnotationManager.GetOrCreateBox(_boxPool, id, _displayLocation, _borderSprite, _font,
                 Color.red);
-            panel.transform.localPosition = new Vector3(box.CenterX, -box.CenterY);
+            panel.transform.localPosition = new Vector3(box.CenterX, box.CenterY);
 
             var rt = panel.GetComponent<RectTransform>();
             rt.sizeDelta = new Vector2(box.Width, box.Height);
